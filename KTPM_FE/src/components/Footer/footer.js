5b76dd73ee14fb216c5c6f62ff9d7bfc8fbd8c61
hieuleedev <hieulee.dev@gmail.com>
2025-05-15 20:30:18 +0700

    fis

--- KTPM_FE/src/components/Footer/footer.js
+++ KTPM_FE/src/components/Footer/footer.js
@@ -1,26 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import "./footer.css";
-import {  faPhone, faSquare } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
-    return ( <>
-    <div id = "footer">
+    return (
+        <>
+            <div id="footer">
+                <div className="footer container">
+                    <div className="footer_section">
+                        <h3>Liên hệ</h3>
+                        <p><FontAwesomeIcon icon={faPhone} /> 0838.300.666</p>
+                        <p><FontAwesomeIcon icon={faEnvelope} /> support@yourclinic.com</p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt} /> 123 Nguyễn Văn Cừ, Q.5, TP.HCM</p>
+                    </div>
 
-        <div className="footer container">
-            <div className="footer_img"></div>
-            <div className="footer_content">
-                <span className="hotline" > HOTLINE : 0838.300.666 </span> <br/>
-                <span>   OR </span> <br/>
-                <span> <FontAwesomeIcon icon={faSquare} /> Contact via facebook </span> <br/>
-                <span> <FontAwesomeIcon icon={faPhone} /> Contact via Zalo </span> <br/>
-            
+                    <div className="footer_section">
+                        <h3>Thời gian hoạt động</h3>
+                        <p>Thứ 2 - Thứ 6: 8:00 - 17:00</p>
+                        <p>Thứ 7: 8:00 - 12:00</p>
+                        <p>Chủ Nhật: Nghỉ</p>
+                    </div>
+
+                    <div className="footer_section">
+                        <h3>Mạng xã hội</h3>
+                        <p><FontAwesomeIcon icon={faFacebook} /> Facebook</p>
+                        <p><FontAwesomeIcon icon={faInstagram} /> Instagram</p>
+                        <p><FontAwesomeIcon icon={faTwitter} /> Twitter</p>
+                    </div>
+
+                    <div className="footer_section">
+                        <h3>Chăm sóc khách hàng</h3>
+                        <p>Hướng dẫn đặt lịch</p>
+                        <p>Câu hỏi thường gặp</p>
+                        <p>Chính sách bảo mật</p>
+                    </div>
+                </div>
+
+                <div className="footer_bottom">
+                    <p>&copy; Hồng Loan. All rights reserved.</p>
+                </div>
             </div>
-        </div>
-    </div>
-    
-    </> );
+        </>
+    );
 }
 
 export default Footer;
-
-
